c029d52dceff23831580f8b712d9774fa379f32e
Jason Howard <jasonh78@gmail.com>
2014-12-08 06:33:06 -0800

    Main function file

---
+++ C++PrimerStudies/main.cpp
@@ -0,0 +1,17 @@
+#include "Ivec.h"
+
+int main()
+{
+	Ivec ivec;
+	//ivec.PrintIvecData();
+	//ivec.SizeOfTest();
+	ivec.LoopTest();
+
+
+
+
+
+	cout << "Please press any key to exit... ";
+	getchar();
+	return 0;
+}
